b8de1e218320f297084d4a92541d5bf86c531aa2
Akshay <akshaytolwanipython@gmail.com>
2019-12-17 16:58:13 +0530

    Fiss

--- shodan-gci.py
+++ shodan-gci.py
@@ -9,15 +9,16 @@ while True:
     print("3.Scanning host")
     print("0.Exit")
     query = input("SHODAN>")
-    if query == 1:
+    query2 = int(query)
+    if query2 == 1:
         os.system("shodan myip")
-    elif query == 2:
+    elif query2 == 2:
         host = input("Search:")
         gg = "shodan search " + host
         os.system(gg)
-    elif query == 3:
+    elif query2 == 3:
         host = input("Host:")
         gg = "shodan scan " + host
         os.system(gg)
-    elif query == 0:
-        exit(0) 
+    elif query2 == 0:
+        exit(0)
