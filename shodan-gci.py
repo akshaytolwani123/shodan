a3658b6551ac287713390a46c2fdca159e3696a7
akshaytolwani123 <57055307+akshaytolwani123@users.noreply.github.com>
2019-12-19 21:37:10 +0530

    Removed os library

--- shodan-gci.py
+++ shodan-gci.py
@@ -1,24 +1,50 @@
+import  shodan
+import socket
 
-import os
+SHODAN_API_KEY = "OwjatLMmmCqakfjyhvvK8mEoC81H5Z9W"
+api = shodan.Shodan(SHODAN_API_KEY)
 
-os.system("shodan init OwjatLMmmCqakfjyhvvK8mEoC81H5Z9W")
-
-while True:
+while  True:
     print("1.What is my ip?")
-    print("2.Scan ip,hostname")
-    print("3.Scanning host")
+    print("2.Scanning Host ")
+    print("3.Shodan search to scan ip,port and hostname")
     print("0.Exit")
     query = input("SHODAN>")
     query2 = int(query)
     if query2 == 1:
-        os.system("shodan myip")
+        ip = socket.gethostbyname(socket.gethostname())
+        print(ip)
     elif query2 == 2:
-        host = input("Search:")
-        gg = "shodan search " + host
-        os.system(gg)
-    elif query2 == 3:
-        host = input("Host:")
-        gg = "shodan scan submit " + host
-        os.system(gg)
+        search = input("Host: ")
+        results = api.host(search)
+        try:
+            for result in results['data']:
+                print(result['ip_str'])
+                print("Open Ports: ", end = "")
+                print(result['port'])
+                print("Operating System: ", end = "")
+                print(result['os'])
+                print("Hostname: ", end = "" )
+                print(result['hostnames'])
+
+
+
+        except shodan.APIError as e:
+            print(e)
+
+    elif  query2 == 3:
+         search = input("Host: ")
+
+         results = api.search(search)
+         try:
+             for result in results['matches']:
+                 print(result['ip_str'], end = "")
+                 print(result['hostnames'])
+
+
+         except shodan.APIError as e:
+             print(e)
     elif query2 == 0:
-        exit(0)
+        print("[x] Exitting the program")
+        exit(code=0)
+
